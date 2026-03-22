857243fb2b6be3f5d4fbd12b286132e82b17f93f
kang-0909 <923870172@qq.com>
2026-03-23 03:49:00 +0800

    Update RouteTone 0.1.0 checksum

--- Casks/routetone.rb
+++ Casks/routetone.rb
@@ -1,6 +1,6 @@
 cask "routetone" do
   version "0.1.0"
-  sha256 "REPLACE_WITH_RELEASE_SHA256"
+  sha256 "bea108ffdbc12133e36a1bbaa2f5821cc3c6faf19b3657b41ab60a7804c4c4d5"
 
   url "https://github.com/kang-0909/RouteTone/releases/download/v#{version}/RouteTone-v#{version}.zip"
   name "RouteTone"
