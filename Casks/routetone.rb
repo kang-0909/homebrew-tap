8992c01582b932f95122a66b830887d2b42ef09a
kang-0909 <923870172@qq.com>
2026-03-23 03:12:57 +0800

    Add RouteTone cask

---
+++ Casks/routetone.rb
@@ -0,0 +1,11 @@
+cask "routetone" do
+  version "0.1.0"
+  sha256 "REPLACE_WITH_RELEASE_SHA256"
+
+  url "https://github.com/kang-0909/RouteTone/releases/download/v#{version}/RouteTone-v#{version}.zip"
+  name "RouteTone"
+  desc "Keep macOS on your preferred audio input and output devices"
+  homepage "https://github.com/kang-0909/RouteTone"
+
+  app "RouteTone.app"
+end
---
+++ README.md
@@ -0,0 +1,20 @@
+# homebrew-tap
+
+Homebrew tap for RouteTone.
+
+## Install
+
+```bash
+brew tap kang-0909/tap
+brew install --cask routetone
+```
+
+## Note
+
+The `routetone` cask expects GitHub releases from:
+
+```text
+https://github.com/kang-0909/RouteTone
+```
+
+After each new RouteTone release, update the cask `version` and `sha256`.
