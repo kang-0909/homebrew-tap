d3354208b96d4adf2bda0001da0fbd8186569232
kang-0909 <923870172@qq.com>
2026-03-24 14:27:19 +0800

    Fix RouteTone 0.1.2 checksum after asset repair

--- Casks/routetone.rb
+++ Casks/routetone.rb
@@ -1,6 +1,6 @@
 cask "routetone" do
   version "0.1.2"
-  sha256 "36b0795e096f9b3349ca0be9e85958bed6d8d435c008b25168d1989563045368"
+  sha256 "1d33bee958ae9e7b38c15b202ff09c517ca33bfc58cb9a266eeae389af2246f9"
 
   url "https://github.com/kang-0909/RouteTone/releases/download/v#{version}/RouteTone-v#{version}.zip"
   name "RouteTone"
