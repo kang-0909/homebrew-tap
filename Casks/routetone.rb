12b31775fcfd9d790ef3d665a50fd07f5fd556f3
kang-0909 <923870172@qq.com>
2026-03-24 15:20:30 +0800

    Point RouteTone cask to 0.1.3 r1 asset

--- Casks/routetone.rb
+++ Casks/routetone.rb
@@ -1,8 +1,8 @@
 cask "routetone" do
   version "0.1.3"
-  sha256 "91ab309ea8d91c5fee3bf535d6dad863fd85210f3fefbddd29bdd9470b61cada"
+  sha256 "3ca3829d2cd03a26d92dadceb56e61a8ac854324a64b856e2a6ba756c01cc3f4"
 
-  url "https://github.com/kang-0909/RouteTone/releases/download/v#{version}/RouteTone-v#{version}.zip"
+  url "https://github.com/kang-0909/RouteTone/releases/download/v#{version}/RouteTone-v#{version}-r1.zip"
   name "RouteTone"
   desc "Keep macOS on your preferred audio input and output devices"
   homepage "https://github.com/kang-0909/RouteTone"
